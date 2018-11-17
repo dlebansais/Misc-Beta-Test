8d83cc19bf3a3161c80c60f139f543474b5ebba0
dlebansais <dlebansais@gmail.com>
2018-11-17 16:51:41 +0100

    Added SimpleDatabase.

---
+++ Test-SimpleDatabase/ConsoleTest - Debug.bat
@@ -0,0 +1,17 @@
+@echo off
+
+if not exist "..\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe" goto error_console
+if not exist "..\Test-SimpleDatabase\bin\x64\Debug\Test-SimpleDatabase.dll" goto error_SimpleDatabase
+"..\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe" --trace=Debug --labels=All "..\Test-SimpleDatabase\bin\x64\Debug\Test-SimpleDatabase.dll"
+pause
+goto end
+
+:error_console
+echo ERROR: nunit3-console not found.
+goto end
+
+:error_SimpleDatabase
+echo ERROR: Test-SimpleDatabase.dll not built.
+goto end
+
+:end
---
+++ Test-SimpleDatabase/TestSet.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Globalization;
+using System.Reflection;
+using System.Threading;
+
+namespace Test
+{
+    [TestFixture]
+    public class TestSet
+    {
+        [OneTimeSetUp]
+        public static void InitTestSession()
+        {
+            CultureInfo enUS = CultureInfo.CreateSpecificCulture("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = enUS;
+            CultureInfo.DefaultThreadCurrentUICulture = enUS;
+            Thread.CurrentThread.CurrentCulture = enUS;
+            Thread.CurrentThread.CurrentUICulture = enUS;
+
+            Assembly SimpleDatabaseAssembly;
+
+            try
+            {
+                SimpleDatabaseAssembly = Assembly.Load("SimpleDatabase");
+            }
+            catch
+            {
+                SimpleDatabaseAssembly = null;
+            }
+            Assume.That(SimpleDatabaseAssembly != null);
+        }
+
+        #region Init
+        [Test]
+        public static void TestInit()
+        {
+            Assert.That(true, "Init");
+        }
+        #endregion
+    }
+}
