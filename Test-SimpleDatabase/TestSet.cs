5e84f9e1ebfb0f5704d250e67803acae7bcd2a0a
dlebansais <dlebansais@gmail.com>
2018-11-18 23:19:57 +0100

    Trying user 'root'

--- Test-SimpleDatabase/TestSet.cs
+++ Test-SimpleDatabase/TestSet.cs
@@ -46,7 +46,7 @@ namespace Test
             }
             else
             {
-                RootId = "travis";
+                RootId = "root";
                 RootPassword = "";
             }
 
