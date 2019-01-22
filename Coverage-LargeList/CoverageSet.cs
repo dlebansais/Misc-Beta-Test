377cfccdb66078d575c4e715c3cec987d295409f
dlebansais <dlebansais@gmail.com>
2019-01-22 10:38:42 +0100

    Added xunit and OpenCover for code coverage.

---
+++ Coverage-LargeList/CoverageSet.cs
@@ -0,0 +1,15 @@
+﻿namespace Coverage
+{
+    using LargeList;
+    using Xunit;
+
+    public class CoverageSet
+    {
+        public static void TestSessionInteger_collection()
+        {
+            LargeList<int> List = new LargeList<int>();
+            List.Add(0);
+            Assert.True(List.Count > 0);
+        }
+    }
+}
