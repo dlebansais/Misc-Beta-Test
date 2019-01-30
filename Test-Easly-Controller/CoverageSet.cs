93c3c0fd286fa9a5ad70a6ccfaa6c917907d20ef
dlebansais <dlebansais@gmail.com>
2019-01-30 15:41:48 +0100

    Updated dependencies.

---
+++ Test-Easly-Controller/CoverageSet.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class CoverageSet
+    {
+        #region Setup
+        [OneTimeSetUp]
+        public static void InitCoverageSession()
+        {
+            CultureInfo enUS = CultureInfo.CreateSpecificCulture("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = enUS;
+            CultureInfo.DefaultThreadCurrentUICulture = enUS;
+            Thread.CurrentThread.CurrentCulture = enUS;
+            Thread.CurrentThread.CurrentUICulture = enUS;
+
+            Assembly EaslyControllerAssembly;
+
+            try
+            {
+                EaslyControllerAssembly = Assembly.Load("Easly-Controller");
+            }
+            catch
+            {
+                EaslyControllerAssembly = null;
+            }
+            Assume.That(EaslyControllerAssembly != null);
+
+        }
+        #endregion
+
+        #region ReadOnly
+        [Test]
+        [Category("Coverage")]
+        public static void ReadOnly()
+        {
+        }
+        #endregion
+    }
+}
