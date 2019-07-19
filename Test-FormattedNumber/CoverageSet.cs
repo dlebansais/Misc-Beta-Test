7562913ba54071c8ea2df0c47f62a492278a119e
dlebansais <dlebansais@gmail.com>
2019-07-19 23:31:33 +0200

    Updated tests.

---
+++ Test-FormattedNumber/CoverageSet.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Threading;
+using FormattedNumber;
+using NUnit.Framework;
+
+namespace Coverage
+{
+    [TestFixture]
+    public class CoverageSet
+    {
+        #region Setup
+        [OneTimeSetUp]
+        public static void InitCoverageSession()
+        {
+            CultureInfo enUS = CultureInfo.CreateSpecificCulture("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = enUS;
+            CultureInfo.DefaultThreadCurrentUICulture = enUS;
+            Thread.CurrentThread.CurrentCulture = enUS;
+            Thread.CurrentThread.CurrentUICulture = enUS;
+
+            Assembly FormattedNumberAssembly;
+
+            try
+            {
+                FormattedNumberAssembly = Assembly.Load("FormattedNumber");
+            }
+            catch
+            {
+                FormattedNumberAssembly = null;
+            }
+            Assume.That(FormattedNumberAssembly != null);
+
+        }
+        #endregion
+
+        #region Basic
+        [Test]
+        [Category("Coverage")]
+        public static void Creation0()
+        {
+            IFormattedNumber n = FormattedNumber.FormattedNumber.Parse("0");
+            Assert.That(n != null, "Sanity Check #0");
+        }
+        #endregion
+    }
+}
